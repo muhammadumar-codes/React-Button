ea56c57afc6ba96c90dde4a1763fa40b7e6ca4ee
muhammadumar-coder <muhammadumar.codes@gmail.com>
2025-09-11 09:39:58 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,19 @@ import Registration from "./Pages/Registration/Registration";
 import User from "./Pages/User-Api/User"; // API calling page
 import Login from "./Pages/Login/Login";
 
+
+
+
+
+// if There is  Not Page Found
+
+function NotFound(){
+return <h1 style={{ color: "red", textAlign: "center" }}>404 - Page Not Found 🚫</h1>
+}
+
+
+// App Export Page.
+
 export default function App() {
   return (
     <>
@@ -22,6 +35,8 @@ export default function App() {
         <Route path="/count" element={<Count />} />
         <Route path="/registration" element={<Registration />} />
         <Route path="/user" element={<User />} />
+        <Route path="*" element={<NotFound/>}/>
+
       </Routes>
 
       <Footer />
